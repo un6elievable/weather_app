bf75718531762a85a4da07006d252526dca89dd3
un6elievable <fenixx0704@gmail.com>
2022-08-16 14:24:44 +0300

    first commit

---
+++ src/API.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '92180bd37bmshc6e8a96353e6b14p1dbda9jsn63a4c13d43e8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'e98fd37c20862cfbdd7d6afab6e9a613';
